64dc945b04a8f8698d217bdfbe3d4b4123f8cd28
carlogilmar <carlogilmar12@gmail.com>
2018-08-12 14:34:31 -0500

    Adding beer get request by id

--- mirage/fixtures/beers.js
+++ mirage/fixtures/beers.js
@@ -1,5 +1,5 @@
 export default [
-  {id:1, name:"Cosaco Guera", user: "@carlogilmar", price: 67, location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"},
-  {id:2, name:"Cosaco Guera", user: "@carlogilmar", price: 67, location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"},
-  {id:3, name:"Cosaco Guera", user: "@carlogilmar", price: 67, location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"}
+  {id:1, name:"Cosaco Guera", user: "@carlogilmar", price: 67, img: "https://goo.gl/T3Zu8n", location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"},
+  {id:2, name:"Cosaco Guera", user: "@carlogilmar", price: 67, img: "https://goo.gl/T3Zu8n", location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"},
+  {id:3, name:"Cosaco Guera", user: "@carlogilmar", price: 67, img: "https://goo.gl/T3Zu8n", location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"}
 ];
