0980484b380619252051274d3c4676ad9461e1e8
carlogilmar <carlogilmar12@gmail.com>
2018-08-12 01:53:09 -0500

    #4 Adding mirage

---
+++ mirage/fixtures/beers.js
@@ -0,0 +1,5 @@
+export default [
+  {id:1, name:"Cosaco Guera", user: "@carlogilmar", price: 67, location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"},
+  {id:2, name:"Cosaco Guera", user: "@carlogilmar", price: 67, location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"},
+  {id:3, name:"Cosaco Guera", user: "@carlogilmar", price: 67, location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"}
+];
