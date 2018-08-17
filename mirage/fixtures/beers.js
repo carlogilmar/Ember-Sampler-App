80554b930af9673301cc3681f0e445f4597e7229
carlogilmar <carlogilmar12@gmail.com>
2018-08-17 00:38:08 -0500

    Updating fixtures

--- mirage/fixtures/beers.js
+++ mirage/fixtures/beers.js
@@ -1,5 +1,5 @@
 export default [
-  {id:1, name:"Cosaco Guera", user: "@carlogilmar", price: 67, img: "https://goo.gl/T3Zu8n", location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"},
-  {id:2, name:"Cosaco Guera", user: "@carlogilmar", price: 67, img: "https://goo.gl/T3Zu8n", location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"},
-  {id:3, name:"Cosaco Guera", user: "@carlogilmar", price: 67, img: "https://goo.gl/T3Zu8n", location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 10, container: "barril", cup: "vaso"}
+  {id:1, name:"Cosaco Guera", user: "@carlogilmar", price: 67, img: "https://goo.gl/T3Zu8n", location: "El Depósito Cuauhtemoc", description: "Sabrosa!", flavour: "IPA", rating: 8, container: "Barril", cup: "Tulipa"},
+  {id:2, name:"Cream Stout", user: "@egjimenezg", price: 79, img: "https://goo.gl/EXWain", location: "Fiebre de Malta", description: "Es una cerveza de mucho cuerpo y con sabor a café.", flavour: "Stout", rating: 10, container: "Botella", cup: "Weizen"},
+  {id:3, name:"Doble IPA", user: "@luissas", price: 67, img: "https://goo.gl/rQAgCr", location: "Hooters Universidad", description: "Es una cerveza muy amarga, y de color poco traslúcido. Es deliciosa!", flavour: "IPA", rating: 10, container: "Barril", cup: "Caliz"}
 ];
