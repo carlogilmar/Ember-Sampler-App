7c4f8f739428303a20e2640c52dcd99bf24494d0
carlogilmar <carlogilmar12@gmail.com>
2018-08-17 01:13:53 -0500

    Adding non-conventional request in mirage

--- mirage/config.js
+++ mirage/config.js
@@ -5,6 +5,8 @@ export default function() {
   this.get('users/current', () => {
     return {
       username: 'carlogilmar',
+      email: 'carlogilmar12@gmail.com',
+      name: 'Carlo Gilmar'
     };
   });
 }
