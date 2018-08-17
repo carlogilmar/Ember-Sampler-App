80e79d296f9ebc26c67c0e45eede971d3a09ec40
carlogilmar <carlogilmar12@gmail.com>
2018-08-17 02:22:00 -0500

    Updating share route

--- app/routes/share.js
+++ app/routes/share.js
@@ -1,23 +1,19 @@
 import Route from '@ember/routing/route';
-import { hash } from 'rsvp';
 
 export default Route.extend({
-    actions:{
-        addBeer:function(){
-            let shareBeer = this.store.createRecord('beer',{
-                name: this.get('controller').get('name'),
-                user: this.get('controller').get('user'),
-                price: this.get('controller').get('price'),
-                img: "https://goo.gl/T3Zu8n",
-                location: this.get('controller').get('location'),
-                description: this.get('controller').get('description'),
-                flavour: this.get('controller').get('flavour'),    
-                rating: this.get('controller').get('rating'),
-                container: "barril",
-                cup: this.get('controller').get('cup')
-            })
-            shareBeer.save();
-            this.transitionTo('index');
-        }
+
+  model(params){
+    let newBeer = this.get('store').createRecord('beer', {
+      user: params.username
+    });
+    return newBeer;
+  },
+
+  actions:{
+    addBeer:function(){
+			let beer = this.currentModel;
+			beer.save()
+      this.transitionTo('index');
     }
+  }
 });
