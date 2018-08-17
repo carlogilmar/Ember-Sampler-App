195c6ecc97f3168cf71372d21308540ad8e029b0
carlogilmar <carlogilmar12@gmail.com>
2018-08-17 02:21:53 -0500

    Updating profile route

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,27 +1,11 @@
 import Route from '@ember/routing/route';
-import ENV from "../config/environment";
 import RSVP from 'rsvp';
-import $ from 'jquery';
 
 export default Route.extend({
-  model(){
-    // Request to non-conventional api
-    let currentUser = new RSVP.Promise(function (resolve, reject) {
-      $.ajax({
-        type: 'GET',
-        url: ENV.rootURL+'users/current',
-        success: function (data) {
-          resolve(data);
-        },
-        error: function (request, textStatus, error) {
-          reject(error);
-        }
-      });
-    });
-
+  model(params){
     return RSVP.hash({
-      currentUser: currentUser,
-      beers: this.get('store').query('beer', {username: "carlogilmar->!"})
+      currentUser: params.username,
+      beers: this.get('store').query('beer', {username: params.username})
     });
   },
 });
