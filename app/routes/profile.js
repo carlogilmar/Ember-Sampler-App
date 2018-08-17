dfd6bd1fb4fb9db2a0d1d3385726df27d6ea6007
carlogilmar <carlogilmar12@gmail.com>
2018-08-17 00:50:08 -0500

    Getting current user in profile route

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
+import ENV from "../config/environment";
+import RSVP from 'rsvp';
+import $ from 'jquery';
 
 export default Route.extend({
+  model(){
+    // Request to non-conventional api
+    return new RSVP.Promise(function (resolve, reject) {
+      $.ajax({
+        type: 'GET',
+        url: ENV.rootURL+'users/current',
+        success: function (data) {
+          resolve(data);
+        },
+        error: function (request, textStatus, error) {
+          reject(error);
+        }
+      });
+    });
+  },
 });
--- mirage/config.js
+++ mirage/config.js
@@ -2,4 +2,9 @@ export default function() {
   this.get('/beers');
   this.get('/beers/:id');
   this.post('/beers');
+  this.get('users/current', () => {
+    return {
+      username: 'carlogilmar',
+    };
+  });
 }
